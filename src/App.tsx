1f517f7ac18ece9ee3e1a11931a96d32bcd1d318
TedYin <jjboy830227@gmail.com>
2025-01-13 00:40:53 +0800

    加入多語系模組，加入資料

--- src/App.tsx
+++ src/App.tsx
@@ -1,32 +1,12 @@
+import Layout from './components/layout/layout';
+
 const App: React.FC = () => {
   return (
-    <div className="app">
-      <header>
-        <div className="flex justify-between items-center">
-          <div className="flex items-center">
-            <img
-              src="https://avatars.githubusercontent.com/u/56169832?v=4"
-              className="w-12 h-12 rounded-full"
-            />
-            <h1 className="text-2xl font-bold ml-2">Your Name</h1>
-          </div>
-        </div>
-        <nav>
-          <ul className="flex justify-center">
-            <li>
-              <a href="#">關於我</a>
-            </li>
-            <li>
-              <a href="#">作品集</a>
-            </li>
-            <li>
-              <a href="#">聯絡我</a>
-            </li>
-          </ul>
-        </nav>
-      </header>
-
-      <main></main>
+    <div className="app w-screen h-screen flex">
+      <Layout />
+      <main className={`w-full h-screen bg-gray-200 dark:bg-gray-900`}>
+        123
+      </main>
     </div>
   );
 };
